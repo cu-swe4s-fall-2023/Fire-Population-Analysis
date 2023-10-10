01ab20aeb5be3db0aed6ef688a8cc6c9c3203369
sarah.auriemma <sarah.auriemma@noaa.gov>
2023-10-10 15:39:26 -0600

    testing CI 4, reformatted `test_my_utils.py` to follow pep8

--- tests/unit_tests/test_my_utils.py
+++ tests/unit_tests/test_my_utils.py
@@ -11,9 +11,12 @@ class TestUtils(unittest.TestCase):
         file_name = '../../Agrofood_co2_emission.csv'
         query_column = 0
         query_value = 'Afghanistan'
-        expected_result = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-                           1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-                           2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+        expected_result = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
+                           1998,
+                           1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+                           2007,
+                           2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015,
+                           2016,
                            2017, 2018, 2019, 2020]
         result = get_column(file_name, query_column, query_value,
                             result_column=1)
